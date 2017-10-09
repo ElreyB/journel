eae5d2fb7e190aa92e1ac294f34be8dcdb8e3085
Elrey Belmonti <elbelmonti@gmail.com>
2017-10-09 14:00:43 -0700

    add some style to page

--- build/js/app.js
+++ build/js/app.js
@@ -53,10 +53,15 @@ $(document).ready(function(){
     var bodyInput = $("textarea#body").val();
     var newEntry = new Entry(titleInput, bodyInput)
 
+    $(".well").addClass("hide");
+    $("#entry").removeClass("hide");
+
     $("span.word_count").text(newEntry.wordCount());
     $("span.vowel_count").text(newEntry.vowelCount());
     $("span.consonant_count").text(newEntry.consonantCount());
     $("span.teaser").text(newEntry.getTeaser());
+    $("h4.title").text(newEntry.heading);
+    $("p.entry").text(newEntry.entry);
   });
 });
 
